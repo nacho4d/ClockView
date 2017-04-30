4401d7b3620035f12063d275034b3c4b927863a8
Guillermo Ignacio Enriquez Gutierrez <ignacio@jp.ibm.com>
2017-04-30 21:26:41 +0900

    New testing target because old was was too old and broken

---
+++ ClockView/ClockViewTest/ClockViewTest.m
@@ -0,0 +1,39 @@
+//
+//  ClockViewTest.m
+//  ClockView
+//
+//  Created by Ignacio on 2017/04/30.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClockViewTest : XCTestCase
+
+@end
+
+@implementation ClockViewTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
